b580d009b04adfa5686faa420bfb0871f2776d26
ray074 <147860006+ray074@users.noreply.github.com>
2023-12-14 00:54:37 +0000

    Update Affine Cipher.py

--- Affine Cipher.py
+++ Affine Cipher.py
@@ -14,24 +14,7 @@ def affineConverter(cipherText, a, b):
     plainText = "".join(final)
     return plainText
             
-def calculateMostProbable(cipherText):
-    alpha, final = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ"), []
-    coprimes = [1, 3, 5, 7, 9, 11, 15, 17, 19, 21, 23, 25]
-    letterDistributions = [
-      8.55, 1.60, 3.16, 3.87, 12.10, 2.18, 2.09, 4.96, 7.33, 0.22, 0.81, 4.21, 2.53,
-      7.17, 7.47, 2.07, 0.10, 6.33, 6.73, 8.94, 2.68, 1.06, 1.83, 0.19, 1.72, 0.11
-    ]
-    
-    for a in coprimes:
-        for b in range(26):
-            result = affineConverter(cipherText, a, b)
-            absDiffs, counts = 0, {}
-            
-            for letter in alpha:
-                value = (result.count(letter) / len(result)) * 100
-                counts[letter] = round(value, 4)
-            
-            for expected, real in zip(letterDistributions, counts.values()):
+def calculateMostProbabln zip(letterDistributions, counts.values()):
                 absDiffs += abs(expected - real)
                 
             final.append(absDiffs)
